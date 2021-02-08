81b421a8f65dd57979d78c19f35d0f1fd731e793
Matthew Smith <matthewsmithwebdesign@gmail.com>
2021-02-08 11:51:25 -0800

    squshing bugs

--- hockeyblog/post/models.py
+++ hockeyblog/post/models.py
@@ -12,12 +12,12 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    content = models.CharField(max_length=200)
+    content = models.TextField(max_length=200)
     image = models.ImageField(upload_to="images")
     cups = models.CharField(max_length=200)
     team_name = models.CharField(max_length=200)
     status = models.IntegerField(choices=STATUS, default=0)
-
+    context_object_name = "posts"
     def __str__(self):
         return self.title
 
