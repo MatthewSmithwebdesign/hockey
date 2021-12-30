bd91d0f8359d4cb3485d168abb979593535adbf0
Matthew Smith <matthewsmithwebdesign@gmail.com>
2021-12-30 09:17:23 -0800

    Remove a hardcoded Django secret key

--- hockeyblog/hockeyblog/settings.py
+++ hockeyblog/hockeyblog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "$1205i!#vsu4zvt8jm*5tnt00_m(-n52gx1rt%gn_qpz(wstgr"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
